8b882959c517293823035d8ecb6e1a61c0321b29
Chabry <krkoc86@gmail.com>
2025-02-24 09:05:31 -0300

    adding .gitignore

--- obj/Debug/net8.0/SauceDemoShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/SauceDemoShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SauceDemoShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ca00224bf2879273250236fc964628998f96e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SauceDemoShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SauceDemoShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
